736f9932ee37dc49d261c4afe99be0a509d0dceb
Sahil <sahilbondre@gmail.com>
2019-10-20 18:10:51 +0530

    Add basic routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import StreamShow from './streams/StreamShow'
+import StreamEdit from './streams/StreamEdit'
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
 
 const App = () => {
   return (
     <div>
-      App
+      <Router>
+        <Route path='/' exact component={StreamList} />
+        <Route path='/streams/new' exact component={StreamCreate} />
+        <Route path='/streams/show' exact component={StreamShow} />
+        <Route path='/streams/edit' exact component={StreamEdit} />
+        <Route path='/streams/delete' exact component={StreamDelete} />
+      </Router>
     </div>
   )
 }
